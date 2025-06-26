471562430452ecc76f6b3fa6745187a2ece3af3d
Dante Nicola Capobianco <dante.capobianco17@gmail.com>
2025-06-26 12:54:56 -0700

    Implement styling of login page, implement form logic to retrieve username/email & password on form submit, implement frontend validation by displaying error if no username/email or password is entered

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,29 +2,66 @@ import { useState } from "react";
 
 const Login = () => {
   const [emailUsername, setEmailUsername] = useState("");
+  const [emailUsernameError, setEmailUsernameError] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const EMAIL_USERNAME_EMPTY_ERROR = "Email or username is required";
+  const PASSWORD_EMPTY_ERROR = "Password is required";
+
+  const login = (event) => {
+    event.preventDefault();
+    setEmailUsernameError("");
+    setPasswordError("");
+
+    if (!emailUsername) {
+      setEmailUsernameError(EMAIL_USERNAME_EMPTY_ERROR);
+      return;
+    } else if (!password) {
+      setPasswordError(PASSWORD_EMPTY_ERROR);
+      return;
+    }
+  };
 
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={(event) => login(event)}>
       <h1 className="login-form-title">Login</h1>
-      <label htmlFor="email-username">Email or Username</label>
-      <input
-        id="email-username"
-        type="text"
-        className="text-input"
-        placeholder="Email or Username"
-        value={emailUsername}
-        onChange={(event) => setEmailUsername(event.target.value)}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        type="text"
-        className="text-input"
-        placeholder="Password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
+      <div className="text-input-container">
+        <input
+          type="text"
+          className="text-input"
+          placeholder="Email or Username"
+          value={emailUsername}
+          maxLength={254}
+          onChange={(event) => setEmailUsername(event.target.value)}
+        />
+        <span className="text-input-error">{emailUsernameError}</span>
+      </div>
+
+      <div className="text-input-container">
+        <div className="login-password-container">
+          <input
+            type={isPasswordVisible ? "text" : "password"}
+            className="text-input"
+            placeholder="Password"
+            value={password}
+            maxLength={30}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <span
+            class="material-symbols-outlined login-password-eye-icon"
+            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+          >
+            {isPasswordVisible ? "visibility_off" : "visibility"}
+          </span>
+        </div>
+        <span className="text-input-error">{passwordError}</span>
+      </div>
+
+      <button type="submit" className="form-btn">
+        Continue
+      </button>
     </form>
   );
 };
