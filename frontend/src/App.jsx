1301df31ff329c5f5f909759a65d5d1b4735a31f
Dante Nicola Capobianco <dante.capobianco17@gmail.com>
2025-06-25 18:00:52 -0700

    Implement foundational routing structure, implement header background color styling, implement header/main/footer skeletons, implement frontend login with input capability & minimal styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
+import { Path } from "./utils/enums";
 
 function App() {
-  return <div className="test">This is a test</div>;
+  const routes = createBrowserRouter([
+    {
+      path: Path.LOGIN,
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <></>,
+    },
+    // Todo: https://docs.google.com/document/d/1RS1UnB0mB0aRISJQ50sOUNsElgAoAFGHbdJiBJf_I90/edit?tab=t.0
+  ]);
+
+  return (
+    <div>
+      <header>
+        <div className="glassmorphism"/>
+        {/* Todo: https://docs.google.com/document/d/1RS1UnB0mB0aRISJQ50sOUNsElgAoAFGHbdJiBJf_I90/edit?tab=t.0 */}
+      </header>
+      <main>
+        <RouterProvider router={routes} />
+      </main>
+      <footer>
+        {/* Todo: https://docs.google.com/document/d/1RS1UnB0mB0aRISJQ50sOUNsElgAoAFGHbdJiBJf_I90/edit?tab=t.0 */}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
