41f94076a5f4b836094f2fac6ebfa64ec5e78a3d
Dante Nicola Capobianco <dante.capobianco17@gmail.com>
2025-06-30 21:37:34 -0700

    Add static interests & skills lists with generic & specific skills/interests, implement dropdown menus for interests & skills, refactor dropdown items logic to enable calling it for ECE areas + skills + interests dropdown menus

--- frontend/src/components/DropdownItem.jsx
+++
@@ -1,5 +0,0 @@
-const DropdownItem = (props) => {
-    return (<div>{props.areaKey}</div>)
-}
-
-export default DropdownItem;
--- frontend/src/components/createAccountSteps/CreateAccountStepThree.jsx
+++ frontend/src/components/createAccountSteps/CreateAccountStepThree.jsx
@@ -1,8 +1,27 @@
-import DropdownItem from "../DropdownItem";
-import { ECE_AREAS } from "../../utils/constants";
+import DropdownItems from "../DropdownItems";
+import { ECE_AREAS, SKILLS, INTERESTS } from "../../utils/constants";
 
 const CreateAccountStepThree = (props) => {
   const STEP_TITLE = "Share Your Interests + Current Skills";
+  const ECE_AREAS_TEXT = "ECE Areas (minimum of 2)";
+  const INTERESTS_TEXT = "Interests (minimum of 5)";
+  const SKILLS_TEXT = "Skills (minimum of 5)";
+
+  const handleRemoveEceArea = (eceAreaKey) => {
+    props.setEceAreas(props.eceAreas.filter((area) => area !== eceAreaKey));
+  };
+
+  const handleRemoveInterest = (interestToRemove) => {
+    props.setInterests(
+      props.interests.filter((interest) => interest !== interestToRemove)
+    );
+  };
+
+  const handleRemoveSkill = (skillToRemove) => {
+    props.setSkills(
+      props.skills.filter((skill) => skill !== skillToRemove)
+    );
+  };
 
   const submitStepThree = (event) => {
     event.preventDefault();
@@ -15,7 +34,7 @@ const CreateAccountStepThree = (props) => {
       onSubmit={(event) => submitStepThree(event)}
     >
       <h1 className="create-account-form-title">{STEP_TITLE}</h1>
-      <div className="text-input-container">
+      <div className="text-input-container dropdown-container">
         <select
           value=""
           className="text-input dropdown-input"
@@ -24,7 +43,7 @@ const CreateAccountStepThree = (props) => {
           }
         >
           <option value="" disabled>
-            ECE Areas
+            {ECE_AREAS_TEXT}
           </option>
           {Object.entries(ECE_AREAS)
             .filter(([key]) => !props.eceAreas.includes(key))
@@ -35,11 +54,68 @@ const CreateAccountStepThree = (props) => {
             ))}
         </select>
 
-        <div>
-          {props.eceAreas.map((areaKey) => (
-            <DropdownItem areaKey={areaKey}/>
+        <DropdownItems
+          selectedItems={props.eceAreas}
+          allItems={ECE_AREAS}
+          removeItem={handleRemoveEceArea}
+          isEceAreaDropdown={true}
+        />
+      </div>
+
+      <div className="text-input-container dropdown-container">
+        <select
+          value=""
+          className="text-input dropdown-input"
+          onChange={(event) =>
+            props.setInterests([...props.interests, event.target.value])
+          }
+        >
+          <option value="" disabled>
+            {INTERESTS_TEXT}
+          </option>
+          {INTERESTS.filter(
+            (interest) => !props.interests.includes(interest)
+          ).map((interest, index) => (
+            <option key={index} value={interest}>
+              {interest}
+            </option>
           ))}
-        </div>
+        </select>
+
+        <DropdownItems
+          selectedItems={props.interests}
+          allItems={INTERESTS}
+          removeItem={handleRemoveInterest}
+          isEceAreaDropdown={false}
+        />
+      </div>
+
+      <div className="text-input-container dropdown-container">
+        <select
+          value=""
+          className="text-input dropdown-input"
+          onChange={(event) =>
+            props.setSkills([...props.skills, event.target.value])
+          }
+        >
+          <option value="" disabled>
+            {SKILLS_TEXT}
+          </option>
+          {SKILLS.filter(
+            (skill) => !props.skills.includes(skill)
+          ).map((skill, index) => (
+            <option key={index} value={skill}>
+              {skill}
+            </option>
+          ))}
+        </select>
+
+        <DropdownItems
+          selectedItems={props.skills}
+          allItems={SKILLS}
+          removeItem={handleRemoveSkill}
+          isEceAreaDropdown={false}
+        />
       </div>
 
       <div className="text-input-container create-account-btn">
