4d611512132a322ed74de10e284b14216b9a703d
Dante Nicola Capobianco <dante.capobianco17@gmail.com>
2025-07-23 09:47:28 -0700

    Adjust server running message to avoid sharing local server URL

--- backend/index.js
+++ backend/index.js
@@ -6,5 +6,5 @@ const server = require("./api/server");
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
